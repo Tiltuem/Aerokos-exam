f552a3a1ade2aa86a93ed12def657079e9311cdb
McCayl <realcayl.cl@gmail.com>
2024-08-09 08:28:42 +0400

    add first

---
+++ src/main/java/org/example/FirstTask.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class FirstTask {
+    public static void main(String[] args) {
+        int[] test = new int[]{1, 4, 7, 3, 3, 3, 3, 2, 2, 1, 1};
+        System.out.println(findIndexOfLowestSeq(test));
+    }
+
+    public static int findIndexOfLowestSeq(int[] data) {
+        int counter = 0;
+        int result = 0;
+        int minSeq = Integer.MAX_VALUE;
+        int i;
+        for (i = 1; i != data.length; i++) {
+            if (data[i] == data[i - 1]) {
+                counter++;
+            } else {
+                if (counter + 1 < minSeq && counter + 1 >= 2) {
+                    counter++;
+                    minSeq = counter;
+                    result = i - counter;
+                    counter = 0;
+                }
+            }
+        }
+        if (counter + 1 < minSeq && counter + 1 >= 2) {
+            counter++;
+            minSeq = counter;
+            result = i - counter;
+            counter = 0;
+        }
+
+        return result;
+    }
+}
